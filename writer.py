58b603c23d39e62d2ce9d343fc9aa73af370b991
Alex <78567009+cf-vnkr@users.noreply.github.com>
2024-07-08 17:17:58 +0100

    2024-07-08 17:17

--- writer.py
+++ writer.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import random
 from tasks import build
 import subprocess
+import time
 
 ACCOUNT_ID = os.environ.get("CLOUDFLARE_ACCOUNT_ID")
 AUTH_TOKEN = os.environ.get("CLOUDFLARE_AUTH_TOKEN")
@@ -21,17 +22,20 @@ def return_answer (response):
 
 def write_article (title, title_slug, story, image, today, today_date):
     file_name = f"content/{today_date}-{title_slug}.md"
-    print(file_name)
-    f = open(file_name, "a")
-    f.write(f"Title: {title}\n")
-    f.write(f"Date: {today}\n")
-    f.write("\n")
-    f.write("> This article is AI generated!\n")
-    f.write("\n")
-    f.write(f"![Alt Text](images/{image})\n")
-    f.write("\n")
-    f.write(story)
-    f.close()
+    try:
+        f = open(file_name, "a")
+        f.write(f"Title: {title}\n")
+        f.write(f"Date: {today}\n")
+        f.write("\n")
+        f.write("> This article is AI generated!\n")
+        f.write("\n")
+        f.write(f"![Alt Text](images/{image})\n")
+        f.write("\n")
+        f.write(story)
+        f.close()
+    except Exception as e:
+       # By this way we can know about the type of error occurring
+        print("Couldn't write article to file: ",e)
 
 news_types: list[str] = [
     "technology",
@@ -42,53 +46,80 @@ news_types: list[str] = [
     "gaming"
 ]
 
-news_theme = random.choice(news_types)
+while True:
+    news_theme = random.choice(news_types)
 
-prompt = f"Give me one name for a {news_theme} related news article"
-response = requests.post(
-  f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/@cf/meta/llama-3-8b-instruct",
-    headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
-    json={
-      "messages": [
-        {"role": "system", "content": "You are a professional assistant"},
-        {"role": "user", "content": prompt}
-      ]
-    }
-)
+    prompt = f"Give me one name for a {news_theme} related news article"
+    response = requests.post(
+    f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/@cf/meta/llama-3-8b-instruct",
+        headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
+        json={
+        "messages": [
+            {"role": "system", "content": "You are a professional assistant"},
+            {"role": "user", "content": prompt}
+        ]
+        }
+    )
 
-title = return_answer(response) # TODO: wrap in try except to break the loop
+    try:
+        title = return_answer(response)
+    except Exception as e:
+       # By this way we can know about the type of error occurring
+        print("Couldn't generate title: ",e)
+        continue
 
-title_slug = slugify(title)
+    title_slug = slugify(title)
 
-prompt = f"Create an article with 3 paragraphs named {title}."
-response = requests.post(
-  f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/@cf/meta/llama-3-8b-instruct",
-    headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
-    json={
-      "messages": [
-        {"role": "system", "content": "You are a professional assistant"},
-        {"role": "user", "content": prompt}
-      ]
-    }
-)
+    prompt = f"Create an article with 3 paragraphs named {title}."
+    response = requests.post(
+    f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/@cf/meta/llama-3-8b-instruct",
+        headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
+        json={
+        "messages": [
+            {"role": "system", "content": "You are a professional assistant"},
+            {"role": "user", "content": prompt}
+        ]
+        }
+    )
 
-article = return_answer(response)
+    try:
+        article = return_answer(response)
+    except Exception as e:
+       # By this way we can know about the type of error occurring
+        print("Couldn't generate article body: ",e)
+        continue
 
+    prompt = f"{title}"
+    image_name = f"{today_date}-{title_slug}.png"
+    response = requests.post(
+    f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/@cf/stabilityai/stable-diffusion-xl-base-1.0",
+        headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
+        json={"prompt": prompt}
+    )
+    try:
+        with open(f"content/images/{image_name}", "wb") as f:
+            f.write(response.content)
+    except Exception as e:
+       # By this way we can know about the type of error occurring
+        print("Couldn't generate image: ",e)
+        continue
 
-prompt = f"{title}"
-image_name = f"{today_date}-{title_slug}.png"
-response = requests.post(
-  f"https://api.cloudflare.com/client/v4/accounts/{ACCOUNT_ID}/ai/run/@cf/stabilityai/stable-diffusion-xl-base-1.0",
-    headers={"Authorization": f"Bearer {AUTH_TOKEN}"},
-    json={"prompt": prompt}
-)
-with open(f"content/images/{image_name}", "wb") as f:
-    f.write(response.content)
+    try:
+        write_article(title, title_slug, article, image_name, today, today_date)
+    except Exception as e:
+       # By this way we can know about the type of error occurring
+        print("Couldn't invoke write_article: ",e)
+        continue
 
+    try:
+        subprocess.run(['pelican', 'content'])
+        subprocess.run(['git', 'add', '.'])
+        subprocess.run(['git', 'commit', '-m', today])
+        subprocess.run(['git','push'])
+    except Exception as e:
+       # By this way we can know about the type of error occurring
+        print("Couldn't build or commit: ",e)
+        continue
 
-write_article(title, title_slug, article, image_name, today, today_date)
-
-subprocess.run(['pelican', 'content'])
-subprocess.run(['git', 'add', '.'])
-subprocess.run(['git', 'commit', '-m', today])
-subprocess.run(['git','push'])
+    hour = 36
+    time.sleep(random.randint(6, 36) * hour)
